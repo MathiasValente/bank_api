cba8fadec4a65d2f991e8d037629b88f71c73a74
Mathias Valente <m.mvalente@outlook.com>
2026-04-29 17:33:35 -0300

    Create engine, session factory and defined Base

--- src/database/db.py
+++ src/database/db.py
@@ -1,3 +1,15 @@
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
 from src.core.config import database_settings
 
+
 DB_URL = database_settings.DB_URL
+engine = create_async_engine(url=DB_URL,
+                             echo=True)
+
+SessionLocal = sessionmaker(bind=engine,
+                            class_=AsyncSession,
+                            expire_on_commit=False)
+
+Base = declarative_base
