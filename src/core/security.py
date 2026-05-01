748d294be4cdb80f39b8b4bd23cd33cde6d1e243
Mathias Valente <m.mvalente@outlook.com>
2026-05-01 16:14:01 -0300

    Refactor to ensure security.py module concern properly

--- src/core/security.py
+++ src/core/security.py
@@ -1,17 +1,49 @@
 from datetime import datetime, timedelta, timezone
+from typing import Annotated
 from jose import jwt, JWTError
 
 from passlib.context import CryptContext
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 
 from sqlalchemy import select
 
 from src.core.config import env_vars
 from src.dependencies.db_dependency import db_dependency
-
 from src.models.users import User
 
 pwd_context = CryptContext(schemes=["argon2"],
                            deprecated="auto")
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+form_dependency = Annotated[OAuth2PasswordRequestForm, Depends()]
+token_dependency = Annotated[str, Depends(oauth2_bearer)]
+
+async def get_current_user(token: token_dependency,
+                           db: db_dependency):
+    payload = decode_jwt(token)
+
+    if not payload:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid/Expirated Token",
+                            headers={"WWW-Authenticate": "Bearer"})
+    
+    user_id = payload.get("sub")
+
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid Token")        
+
+    user = await db.get(User, user_id)
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="User not Found!")
+
+    return user
+
+user_dependency = Annotated[User, Depends(get_current_user)]
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
