e66b58e7ab1fd61e6599fb6e8bb19a8b25839c67
Mathias Valente <m.mvalente@outlook.com>
2026-05-04 14:08:01 -0300

    Included all routes in the app

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from contextlib import asynccontextmanager
 from src.database.init_db import init_models
 from src.routes.auth import router as auth_route
 from src.routes.user import router as user_route
-
+from src.routes.accounts import router as acc_route
+from src.routes.transactions import router as transactions_route
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_models()
@@ -15,3 +16,5 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_route)
 app.include_router(auth_route)
+app.include_router(acc_route)
+app.include_router(transactions_route)
